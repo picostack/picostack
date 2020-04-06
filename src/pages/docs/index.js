5b0ee3810d9234480252811062092e9be69a874a
Southclaws <hello@southcla.ws>
2020-04-06 14:22:23 +0100

    fix redirect

--- src/pages/docs/index.js
+++ src/pages/docs/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import { Redirect } from "@docusaurus/router";
 
 export default () => {
-  return <Redirect to="/docs/introduction" />;
+  return <Redirect to="/docs/intro/introduction" />;
 };
